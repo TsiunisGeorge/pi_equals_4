9cbc47ca9cb12961674e326a36dd101284a488f5
TsiunisGeorge <116101101+TsiunisGeorge@users.noreply.github.com>
2025-09-28 16:56:46 +0300

    Update frst.py

--- frst.py
+++ frst.py
@@ -1,4 +1,5 @@
 from pymilvus import MilvusClient
+from sentence_transformers import SentenceTransformer
 
 client = MilvusClient("documents.db")
 
@@ -10,7 +11,20 @@ client.create_collection(
 )
 
 
+from pymilvus import model
+# If connection to https://huggingface.co/ failed, uncomment the following path
+#import os
+#os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
 
+model = SentenceTransformer('cointegrated/rubert-tiny2')
+#model = SentenceTransformer('ai-forever/sbert_large_mt_nlu_ru')
 
+'''
+sentences = ["привет мир", "hello world", "здравствуй вселенная"]
+embeddings = model.encode(sentences)
+print(embeddings)
+'''
 
+# This will download a small embedding model "paraphrase-albert-small-v2" (~50MB).
+embedding_fn = model.encode
